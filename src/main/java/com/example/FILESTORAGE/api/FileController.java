2972c99b73ba8a8af3f55189c11b22c77615caa7
M1yamura <skelet0182@gmail.com>
2026-04-25 20:24:45 +0300

    small improvements

--- src/main/java/com/example/FILESTORAGE/api/FileController.java
+++ src/main/java/com/example/FILESTORAGE/api/FileController.java
@@ -2,47 +2,44 @@ package com.example.FILESTORAGE.api;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.List;
-
-
+import java.util.UUID;
 
 @Controller
-@RequestMapping("/files")
 public class FileController {
     final Path basePath;
-
     public FileController(@Value("${storage.root}")Path path) {
         this.basePath = path;
     }
 
-    @GetMapping
-    public String files() {
-        return "files";
-    }
     //Нужно добавить автоген имён файлов
     @PostMapping("/upload")
     public String handleUpload(@RequestParam("files") List<MultipartFile> files) throws IOException {
         if (!files.isEmpty()) {
             for (MultipartFile file : files) {
+
+                //извлечение расширения файла
+                //--------------------------------------------------------
+                String originalFilename = file.getOriginalFilename();
+                String extension = "";
+                if (originalFilename != null && originalFilename.contains(".")) {
+                    extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+                }//-------------------------------------------------------
+
+
                 Files.copy(file.getInputStream(),
-                        Paths.get(basePath.toString(), file.getOriginalFilename()).normalize(),
+                        basePath.resolve(UUID.randomUUID().toString() + extension).normalize(),
                         StandardCopyOption.REPLACE_EXISTING );
             }
 
         }
-        return "redirect:/files";
+        return "redirect:/index.html";
     }
-
-
-
 }
